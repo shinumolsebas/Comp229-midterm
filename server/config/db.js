079b970d860daec9d1cbdafa6f1641b88c03ac66
Shinumol Sebastian <shinumolsebas@gmail.com>
2023-06-19 23:46:07 -0300

    mongodb added

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,7 @@
 module.exports = {
   //local MongoDB deployment ->
  // "URI": "mongodb://localhost/books229"
- "URI": "mongodb://127.0.0.1/books229"
+ //"URI": "mongodb://127.0.0.1/books229"
+
+ "URI":"mongodb+srv://shinualepy:TN75KMUFi2GST4Jz@midterm.nnnc7fy.mongodb.net/books229?retryWrites=true&w=majority"
 };
